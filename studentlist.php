c2681ee9c77b124312253789ff4b824c25e85dd3
Akshay Kadam <46850344+Akshaykadam66@users.noreply.github.com>
2020-05-16 17:05:03 +0530

    Add files via upload

---
+++ studentlist.php
@@ -0,0 +1,23 @@
+<?php
+	$myfile = fopen('studentdetails.txt', 'r') or die("Unable to open file!");
+	 echo "SRN . 	|    NAME .   |	  EMAILID .   |	   CGPA .";	 
+	echo "<br>";  
+	while(!feof($myfile)){
+ 	 	$filedata = fgets($myfile);
+		echo $filedata;
+		echo "<br>";
+	}	
+	fclose($myfile);
+?>
+<html>
+<body>
+	<form method="POST" action="smtp.py">
+        
+	<label><b>Enter Recepeient EmailID</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text" value={{request.form.username}} name="txtemailid"  placeholder="Enter Emialid" required/>
+	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="reset" name="btnreset" value="RESET" class="button" required>
+                    <input type="submit" name="btnreg" value="Countinue" class="button" >
+</form>
+</body>
+</html>             
